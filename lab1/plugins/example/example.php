5db39fd301dd6e8c8d5dde589236dd01b1cd6333
Yuliaskor <yuliaskor@ukr.net>
2023-03-04 19:15:59 +0100

    lab 1

---
+++ lab1/plugins/example/example.php
@@ -0,0 +1,28 @@
+<?php
+/*
+Plugin Name: Example
+Plugin URI: 
+Description: Example
+Version: 0.1.0
+Author: Yuliia Skorbach
+Author URI: 
+*/
+
+//Security
+if(!defined('ABSPATH')){
+    exit;
+  }
+
+// Load Scripts
+require_once(plugin_dir_path(__FILE__).'/includes/example-scripts.php');
+
+// Load Class
+require_once(plugin_dir_path(__FILE__).'/includes/example-class.php');
+
+// Register Widget
+function register_examplesubs(){
+    register_widget('Example_Subs_Widget');
+  }
+  
+//Hook in function
+add_action('widgets_init', 'register_examplesubs');
