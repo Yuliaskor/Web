3b947a16982a30ddbf9c51b1fb5b57b5ac3d9be2
Yuliaskor <yuliaskor@ukr.net>
2023-04-03 19:58:12 +0200

    paginacja

--- lab6/library-frontend/src/components/books/BooksListComponent.vue
+++ lab6/library-frontend/src/components/books/BooksListComponent.vue
@@ -13,24 +13,66 @@
           </tr>
         </thead>
         <tbody>
-          <BookComponent v-for="book in books" :key="book.id" :book="book"/>
+          <BookComponent v-for="book in paginatedBooks" :key="book.id" :book="book"/>
         </tbody>
       </table>
+      <Paginate
+        :page-count="pageCount"
+        :page-range="3"
+        :margin-pages="1"
+        :click-handler="paginate"
+        :prev-text="'Previous'"
+        :next-text="'Next'"
+        :container-class="'pagination justify-content-center'"
+        :page-class="'page-item'"
+        :page-link-class="'page-link'"
+        :prev-class="'page-item'"
+        :prev-link-class="'page-link'"
+        :next-class="'page-item'"
+        :next-link-class="'page-link'"
+      />
+        <nav aria-label="Books Pagination">
+        <ul class="pagination">
+          <li class="page-item" :class="{ 'disabled': currentPage === 1 }">
+            <button class="page-link" @click="currentPage -= 1">Previous</button>
+          </li>
+          <li class="page-item" v-for="page in pages" :key="page" :class="{ 'active': currentPage === page }">
+            <button class="page-link" @click="currentPage = page">{{ page }}</button>
+          </li>
+          <li class="page-item" :class="{ 'disabled': currentPage === totalPages }">
+            <button class="page-link" @click="currentPage += 1">Next</button>
+          </li>
+        </ul>
+      </nav>
       </div>
   </div>
 </template>
 
 <script>
 import BookComponent from './BookComponent'
+import Paginate from 'vuejs-paginate'
 
 export default {
   name: 'BooksList',
   components: {
-    BookComponent
+    BookComponent,
+    paginate: Paginate
   },
   data() {
     return {
-      books: []
+      books: [],
+      perPage: 10,
+      currentPage: 1
+    }
+  },
+  computed: {
+    pageCount() {
+      return Math.ceil(this.books.length / this.perPage)
+    },
+    paginatedBooks() {
+      const start = (this.currentPage - 1) * this.perPage
+      const end = start + this.perPage
+      return this.books.slice(start, end)
     }
   },
   mounted() {
@@ -45,6 +87,9 @@ export default {
       } catch (error) {
         console.log(error)
       }
+    },
+    paginate(event) {
+      this.currentPage = event
     }
   }
 }
