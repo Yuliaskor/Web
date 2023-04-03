83f0c10f410a1ec7adf3cb21226cf127b82cc226
Yuliaskor <yuliaskor@ukr.net>
2023-04-03 19:27:47 +0200

    wypozycianie

--- lab6/library-frontend/src/components/books/BookComponent.vue
+++ lab6/library-frontend/src/components/books/BookComponent.vue
@@ -5,10 +5,18 @@
         <td>{{ book.publisher }}</td>
         <td>{{ book.publicationYear }}</td>
         <td>
+          <div v-if="book.borrow">
+            <span>Wypożyczona</span>
+          </div>
+          <div v-else>
+            <button @click="borrowBook(book.id)" class="btn btn-success mx-1 mb-3">
+              <font-awesome-icon icon="check" /> Wypożycz
+            </button>
+          </div>
             <button @click="deleteBook(book.id)" class="btn btn-danger mx-1">
-    <font-awesome-icon icon="trash" /></button>
+                  <font-awesome-icon icon="trash" /></button>
             <router-link :to="{ name: 'EditBook', params: { id: book.id } }"><button class="btn btn-secondary mx-1">
-      <font-awesome-icon icon="edit" /></button></router-link>
+                 <font-awesome-icon icon="edit" /></button></router-link>
         </td>
     </tr>
 </template>
@@ -34,7 +42,18 @@ export default {
       } catch (error) {
         console.log(error)
       }
-    }
-  }
-}
+    },
+    async borrowBook(bookId) {
+      console.log('borrowing book with id ' + bookId);
+      try {
+        await fetch('http://localhost:8080/api/v1/rentals/' + bookId + '/1', {
+          method: 'POST',
+        });
+        location.reload();
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
 </script>
